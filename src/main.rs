8116ad89bc17d81cfbfb599b5cc1897feb691270
Simon Haugen <smn.hgn@gmail.com>
2022-07-02 14:51:48 +0200

    move structs to mod models

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,8 @@
-use serde::{Deserialize, Serialize};
+mod models;
+
+use models::PackageLock;
 use serde_json::{Error};
-use std::{env, fs, path::Path, collections::HashMap};
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct Package {
-    version: Option<String>,
-    integrity: Option<String>,
-    resolved: Option<String>,
-    dev: Option<bool>,
-    optional: Option<bool>,
-    dev_optional: Option<bool>,
-    name: Option<String>,
-    in_bundle: Option<bool>,
-    has_install_script: Option<bool>,
-    has_shrink_wrap: Option<bool>,
-    license: Option<String>,
-    bin: Option<HashMap<String, String>>,
-    engines: Option<HashMap<String, String>>,
-    dependencies: Option<HashMap<String, String>>,
-    dev_dependencies: Option<HashMap<String, String>>,
-    optional_dependencies: Option<HashMap<String, String>>,
-}
-
-#[derive(Serialize, Deserialize, Debug)]
-#[serde(rename_all = "camelCase")]
-struct PackageLock {
-    name: String,
-    version: Option<String>,
-    lockfile_version: i8,
-    packages: HashMap<String, Package>,
-    // dependencies: Record<string, Dependency>;
-}
+use std::{env, fs, path::Path};
 
 fn read_package_lock_json(json_path: &str) -> Result<PackageLock, Error> {
     let json_file_path = Path::new(json_path).join("package-lock.json");
@@ -47,7 +18,8 @@ fn main() {
         _ => ".",
     };
 
-    if let Ok(package_lock) = read_package_lock_json(json_path) {
-        println!("{:?}", package_lock);
+    match read_package_lock_json(json_path) {
+        Ok(package_lock) => println!("{:#?}", package_lock),
+        Err(err) => panic!("invalid json: {}", err.to_string()),
     };
 }
---
+++ src/models.rs
@@ -0,0 +1,49 @@
+// https://docs.npmjs.com/cli/v8/configuring-npm/package-lock-json#file-format
+
+use std::collections::HashMap;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Package {
+    version: Option<String>,
+    integrity: Option<String>,
+    resolved: Option<String>,
+    dev: Option<bool>,
+    optional: Option<bool>,
+    dev_optional: Option<bool>,
+    name: Option<String>,
+    in_bundle: Option<bool>,
+    has_install_script: Option<bool>,
+    has_shrink_wrap: Option<bool>,
+    license: Option<String>,
+    bin: Option<HashMap<String, String>>,
+    engines: Option<HashMap<String, String>>,
+    dependencies: Option<HashMap<String, String>>,
+    dev_dependencies: Option<HashMap<String, String>>,
+    optional_dependencies: Option<HashMap<String, String>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+struct Dependency {
+    version: Option<String>,
+    integrity: Option<String>,
+    resolved: Option<String>,
+    dev: Option<bool>,
+    optional: Option<bool>,
+    dev_optional: Option<bool>,
+    bundled: Option<bool>,
+    requires: Option<HashMap<String, String>>,
+    dependencies: Option<HashMap<String, Dependency>>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PackageLock {
+    name: String,
+    version: Option<String>,
+    lockfile_version: i8,
+    packages: HashMap<String, Package>,
+    dependencies: Option<HashMap<String, Dependency>>,
+}
